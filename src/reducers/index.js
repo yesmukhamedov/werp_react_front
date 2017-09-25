373755403bde618961fe1b4371b12cd26c488f69
azokarta <azokarta1988@gmail.com>
2017-09-25 11:57:22 +0600

    add userBranch reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import authReducer from './auth_reducer';
 import usersReducer from './users';
 import langReducer from './lang_reducer';
 import inboxReducer from './inbox';
+import ditUserBranchReducer from './dit/userBranch_reducer';
 
 const rootReducer = combineReducers({
   form,
   auth: authReducer,
   users: usersReducer,
   locales: langReducer,
+  ditUserBranch: ditUserBranchReducer,  
   inbox: inboxReducer
 });
 
