a2f1cb62c12ac63c9036acecd3bbe14bb1c3d567
nurgissa <nurgisa@gmail.com>
2017-11-21 22:28:44 +0600

    Add complete implementation of SpView

--- src/service/mainoperation/spView/components/spViewPage.js
+++ src/service/mainoperation/spView/components/spViewPage.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
-import {Button, Table, Icon, Label, Input, Form, Dropdown, Grid, Header, Segment, Container} from 'semantic-ui-react'
+import {Table, Label, Input, Form, Grid, Header, Segment, Container} from 'semantic-ui-react'
 import axios from 'axios'
+import moment from 'moment'
 import {ROOT_URL} from '../../../../utils/constants'
 
 export default class extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            servicePacketData: undefined
+            servicePacketData: {}
         }
     }
 
@@ -27,6 +28,26 @@ export default class extends Component {
     }
 
     render() {
+        const {
+            servicePacket = {},
+            spareParts = [],
+            sparePartsWithWarranty = []
+        } = this.state.servicePacketData
+
+        const {
+            country = {},
+            company = {},
+            name = '',
+            description = '',
+            product = {},
+            productCategory = {},
+            startDate = '',
+            totalPrice = '',
+            masterBonus = '',
+            operatorBonus = ''
+        } = servicePacket
+    
+
         return (            
             <Container fluid style={{marginTop: '2em', marginBottom: '2em', paddingLeft: '2em', paddingRight: '2em'}}>
                 <Form>
@@ -39,15 +60,31 @@ export default class extends Component {
                                 <Grid.Column width={3}>
                                     <Form.Field>
                                         <label>Компания</label>
+                                        <Input
+                                            type='text'
+                                            placeholder='Название компании'
+                                            value={company.name} />
                                     </Form.Field>
                                     <Form.Field>
                                         <label>Страна</label>
+                                        <Input
+                                            type='text'
+                                            placeholder='Название страны'
+                                            value={country.country} />
                                     </Form.Field>
                                     <Form.Field>
                                         <label>Категория</label>
+                                        <Input
+                                            type='text'
+                                            placeholder='Название категории'
+                                            value={productCategory.label} />
                                     </Form.Field>
                                     <Form.Field>
                                         <label>Товар</label>
+                                        <Input
+                                            type='text'
+                                            placeholder='Название товара'
+                                            value={product.name} />
                                     </Form.Field>
                                 </Grid.Column>
                                 <Grid.Column width={5}>
@@ -55,19 +92,22 @@ export default class extends Component {
                                         <label>Название</label>
                                         <Input
                                             type='text'
-                                            placeholder='Название' />
+                                            placeholder='Название сервис пакета'
+                                            value={name} />
                                     </Form.Field>
                                     <Form.Field>
                                         <label>Примечание</label>
                                         <Input
                                             type='text'
-                                            placeholder='Примечание' />
+                                            placeholder='Описание сервис пакета'
+                                            value={description} />
                                     </Form.Field>
                                     <Form.Field>
                                         <label>Дата начала действия</label>
                                         <Input
                                             type='text'
-                                            placeholder='Примечание' />
+                                            placeholder='Примечание'
+                                            value={moment(startDate).format('DD.MM.YYYY')} />
                                     </Form.Field>
                                 </Grid.Column>
                             </Grid.Row>
@@ -78,34 +118,46 @@ export default class extends Component {
                 <Table celled color='black' striped>
                     <Table.Header>
                         <Table.Row>
-                            <Table.HeaderCell/>
-                            <Table.HeaderCell colSpan='8'></Table.HeaderCell>
-                        </Table.Row>
-                        <Table.Row>
                             <Table.HeaderCell collapsing>#</Table.HeaderCell>
                             <Table.HeaderCell>Операция</Table.HeaderCell>
-                            <Table.HeaderCell collapsing>Материал</Table.HeaderCell>
                             <Table.HeaderCell>Код</Table.HeaderCell>
                             <Table.HeaderCell>Описание</Table.HeaderCell>
                             <Table.HeaderCell>Цена</Table.HeaderCell>
                             <Table.HeaderCell>Количество (шт.)</Table.HeaderCell>
                             <Table.HeaderCell>Сумма</Table.HeaderCell>
-                            <Table.HeaderCell collapsing></Table.HeaderCell>
                         </Table.Row>
                     </Table.Header>
                     <Table.Body>
+                        {spareParts.map((item, idx) => {
+                            const {
+                                operType,
+                                code = '',
+                                price = '',
+                                description = '',
+                                quantity = '',
+                                totalPrice = ''
+                            } = item
+                            const {operName=''} = operType
+                            return (
+                                <Table.Row>
+                                    <Table.Cell>{idx+1}</Table.Cell>
+                                    <Table.Cell>{operName}</Table.Cell>
+                                    <Table.Cell>{code}</Table.Cell>
+                                    <Table.Cell>{description}</Table.Cell>
+                                    <Table.Cell>{price}</Table.Cell>
+                                    <Table.Cell>{quantity}</Table.Cell>
+                                    <Table.Cell>{totalPrice}</Table.Cell>
+                                </Table.Row>
+                            )}
+                        )}
                     </Table.Body>
                     <Table.Footer fullWidth>
                         <Table.Row>
                             <Table.HeaderCell
-                                colSpan='8'
+                                colSpan='6'
                                 style={{
-                                textAlign: 'right'
-                            }}>Total</Table.HeaderCell>
-                            <Table.HeaderCell
-                                style={{
-                                textAlign: 'center'
-                            }}></Table.HeaderCell>
+                                textAlign: 'right'}}>Total</Table.HeaderCell>
+                            <Table.HeaderCell>{totalPrice}</Table.HeaderCell>
                         </Table.Row>
                     </Table.Footer>
                 </Table>
@@ -113,18 +165,28 @@ export default class extends Component {
                 <Table celled color='black' striped>
                     <Table.Header>
                         <Table.Row>
-                            <Table.HeaderCell colSpan='6'/>
-                        </Table.Row>
-                        <Table.Row>
                             <Table.HeaderCell collapsing>#</Table.HeaderCell>
-                            <Table.HeaderCell collapsing>Материал</Table.HeaderCell>
                             <Table.HeaderCell>Код</Table.HeaderCell>
                             <Table.HeaderCell>Описание</Table.HeaderCell>
                             <Table.HeaderCell>Гарантия (в мес.)</Table.HeaderCell>
-                            <Table.HeaderCell collapsing></Table.HeaderCell>
                         </Table.Row>
                     </Table.Header>
                     <Table.Body>
+                        {sparePartsWithWarranty.map((item, idx) => {
+                            const {
+                                code = '',
+                                description = '',
+                                warrantyMonths = ''
+                            } = item
+                            return (
+                                <Table.Row>
+                                    <Table.Cell>{idx+1}</Table.Cell>
+                                    <Table.Cell>{code}</Table.Cell>
+                                    <Table.Cell>{description}</Table.Cell>
+                                    <Table.Cell>{warrantyMonths}</Table.Cell>
+                                </Table.Row>
+                            )
+                        })}
                     </Table.Body>
                 </Table>
 
@@ -133,13 +195,17 @@ export default class extends Component {
                         <Grid.Column>
                             <Segment padded>
                                 <Form>
-                                    <Form.Input label='Сумма к оплате' placeholder='Сумма к оплате'/>
+                                    <Form.Input label='Сумма к оплате' 
+                                        placeholder='Сумма к оплате'
+                                        value={totalPrice} />
                                     <Form.Input
                                         label='Премия Мастера'
-                                        placeholder='Премия Мастера' />
+                                        placeholder='Премия Мастера'
+                                        value={masterBonus} />
                                     <Form.Input
                                         label='Премия оператора'
-                                        placeholder='Премия оператора' />
+                                        placeholder='Премия оператора'
+                                        value={operatorBonus} />
                                 </Form>
                             </Segment>
                         </Grid.Column>
