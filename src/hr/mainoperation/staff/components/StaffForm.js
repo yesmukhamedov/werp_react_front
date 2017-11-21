26139663a55a7508759cd22d9588be2822aa1f9a
onlasyn <onlasyn@gmail.com>
2017-11-21 16:18:27 +0600

    Hr->StaffView

--- src/hr/mainoperation/staff/components/StaffForm.js
+++ src/hr/mainoperation/staff/components/StaffForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import {Container,Form,Button,Checkbox,Divider} from 'semantic-ui-react';
+import {Form,Button,Checkbox,Divider} from 'semantic-ui-react';
 import DatePicker from "react-datepicker";
 import {ROOT_URL} from '../../../../utils/constants';
 
@@ -15,12 +15,51 @@ class StaffForm extends Component{
         this.state = {
             staff:{
                 firstname:'',
-                middlename:''
+                middlename:'',
+                lastname:'',
+                passport_id:'',
+                passport_given_by:'',
+                passport_given_date:null,
+                birthday:null,
+                gender:0,
+                passport_id:'',
+                passport_given_by:'',
+                passport_given_date:null,
+                passport_validity:null,
+                passport_id2:'',
+                passport_given_by2:'',
+                passport_given_date2:null,
+                passport_validity2:null,
+                homephone:'',
+                workphone:'',
+                mobile:'',
+                mobile1:'',
+                corp_email:'',
+                email:'',
+                email2:'',
+                regAddress:{
+                    country_id:0,
+                    state_id:0,
+                    city_id:0,
+                    region_id:0,
+                    postal_code:'',
+                    microdistrict:'',
+                    village:'',
+                    avenue:'',
+                    street:'',
+                    ap_number:'',
+                    ap_drob:'',
+                    flat_number:''
+                }
             },
-            countryOptions: [],
-            stateOptions: [],
-            cityOptions:[],
-            regionOptions:[],
+            countryOptions1: [],
+            stateOptions1: [],
+            cityOptions1:[],
+            regionOptions1:[],
+            countryOptions2: [],
+            stateOptions2: [],
+            cityOptions2:[],
+            regionOptions1:[],
             sendingData:false
         }
 
@@ -130,13 +169,31 @@ class StaffForm extends Component{
             ...this.state,
             sendingData:true
         })
+
+        axios.post(`${ROOT_URL}/api/hr/staff`,this.state.staff)
+            .then((response) => {
+                    console.log(response);
+            }).catch((error) => {
+                console.log(error);
+        })
+    }
+
+    onInputChange(fieldName,fieldValue){
+        const stf = this.state.staff;
+        if(stf.hasOwnProperty(fieldName)){
+            stf[fieldName] = fieldValue;
+        }
+
+        this.setState({
+            ...this.state,
+            staff:stf
+        })
     }
 
 
     render(){
         return (
-            <Container fluid style={{ marginTop: '2em', marginBottom: '2em', paddingLeft: '2em', paddingRight: '2em'}}>
-                <h2>Добавление нового сотрудника</h2>
+
                 <Form>
                     <div className="ui segments">
                         <div className="ui segment">
@@ -144,21 +201,29 @@ class StaffForm extends Component{
                         </div>
                         <div className="ui secondary segment">
                             <Form.Group widths='equal'>
-                                <Form.Input label='Фамилия' placeholder='Фамилия' />
-                                <Form.Input label='Имя' placeholder='Имя' />
-                                <Form.Input label='Отчество' placeholder='Отчество' />
+                                <Form.Input label='Фамилия' placeholder='Фамилия'
+                                            onChange={e => this.onInputChange("lastname",e.target.value)} />
+                                <Form.Input label='Имя' placeholder='Имя'
+                                            onChange={e => this.onInputChange("firstname",e.target.value)}/>
+                                <Form.Input label='Отчество' placeholder='Отчество'
+                                            onChange={e => this.onInputChange("middlename",e.target.value)}/>
                             </Form.Group>
 
                             <Form.Group widths='equal'>
-                                <Form.Input label='ИИН' placeholder='ИИН' />
+                                <Form.Input label='ИИН' placeholder='ИИН'
+                                            onChange={e => this.onInputChange("iin",e.target.value)}/>
                                 <Form.Field>
                                     <label>Дата рождения</label>
                                     <DatePicker
                                         placeholderText={'Дата рождения'}
                                         showMonthDropdown showYearDropdown dropdownMode="select"
-                                        dateFormat="DD.MM.YYYY" />
+                                        selected={this.state.staff.birthday}
+                                        dateFormat="DD.MM.YYYY"
+                                        onChange={(v) => this.onInputChange("birthday",v)}
+                                        />
                                 </Form.Field>
-                                <Form.Select label='Пол' options={genderOptions} placeholder='Пол' />
+                                <Form.Select label='Пол' options={genderOptions} placeholder='Пол'
+                                             onChange={e => this.onInputChange("gender",e.target.value)}/>
                             </Form.Group>
                         </div>
                     </div>
@@ -247,16 +312,49 @@ class StaffForm extends Component{
                             </Form.Group>
 
                             <Form.Group widths='equal'>
-                                <Form.Input label='Корпоративный email' placeholder='Корпоративный email' />
-                                <Form.Input label='Личный email' placeholder='Личный email' />
-                                <Form.Input label='Доп. email' placeholder='Доп. email' />
+                                <Form.Input label='Почтовый индекс' placeholder='Почтовый индекс' />
+                                <Form.Input label='Микрорайон' placeholder='Микрорайон' />
+                                <Form.Input label='Поселок' placeholder='Поселок' />
+                            </Form.Group>
+
+                            <Form.Group widths='equal'>
+                                <Form.Input label='Проспект' placeholder='Проспект' />
+                                <Form.Input label='Ул.(просп./мкр.)' placeholder='Ул.(просп./мкр.)' />
+                                <Form.Input label='Дом' placeholder='Дом' />
+                                <Form.Input label='Квартира №' placeholder='Квартира №' />
+                            </Form.Group>
+                        </div>
+                    </div>
+
+                    <div className="ui segments">
+                        <div className="ui segment">
+                            <h3>Место проживание</h3>
+                        </div>
+                        <div className="ui secondary segment">
+                            <Form.Group widths='equal'>
+                                <Form.Select onChange={(e, { value }) => this.loadStates(value)} label='Страна' options={this.state.countryOptions} placeholder='Страна' />
+                                <Form.Select onChange={(e, { value }) => this.loadCities(value)}  label='Область' options={this.state.stateOptions} placeholder='Область' />
+                                <Form.Select onChange={(e, { value }) => this.loadRegions(value)} label='Город' options={this.state.cityOptions} placeholder='Город' />
+                                <Form.Select label='Район' options={this.state.regionOptions} placeholder='Район' />
+                            </Form.Group>
+
+                            <Form.Group widths='equal'>
+                                <Form.Input label='Почтовый индекс' placeholder='Почтовый индекс' />
+                                <Form.Input label='Микрорайон' placeholder='Микрорайон' />
+                                <Form.Input label='Поселок' placeholder='Поселок' />
+                            </Form.Group>
+
+                            <Form.Group widths='equal'>
+                                <Form.Input label='Проспект' placeholder='Проспект' />
+                                <Form.Input label='Ул.(просп./мкр.)' placeholder='Ул.(просп./мкр.)' />
+                                <Form.Input label='Дом' placeholder='Дом' />
+                                <Form.Input label='Квартира №' placeholder='Квартира №' />
                             </Form.Group>
                         </div>
                     </div>
 
                     <Button onClick={this.submitForm} className={this.state.sendingData ?'loading':''} color='teal'>Сохранить</Button>
                 </Form>
-            </Container>
         )
     }
 }
---
+++ src/hr/mainoperation/staff/components/ViewStaff.js
@@ -0,0 +1,332 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import moment from 'moment'
+import {Container,Divider,Tab,Table,Grid,Label,Menu,Icon,Header} from 'semantic-ui-react';
+import {ROOT_URL} from '../../../../utils/constants';
+
+class ViewStaff extends Component{
+    constructor(props) {
+        super(props)
+        this.loadedSuccess = true;
+        this.state = {
+            staff:{},
+            salaries:[],
+            salariesLoaded:false,
+            expenses:[],
+            expensesLoaded:false
+        }
+
+        this.renderMainData = this.renderMainData.bind(this);
+        this.renderPassportData = this.renderPassportData.bind(this);
+        this.renderSalaryData = this.renderSalaryData.bind(this);
+        this.handleTabChange = this.handleTabChange.bind(this);
+        this.renderContactData = this.renderContactData.bind(this);
+        this.renderExpensesData = this.renderExpensesData.bind(this);
+    }
+
+    componentWillMount(){
+        let _this = this;
+        axios.get(`${ROOT_URL}/api/hr/staff/` + this.props.params.id,{
+            headers: {
+                authorization: localStorage.getItem('token')}
+        }).then((response) => {
+            this.setState({
+                ...this.state,
+                staff:response.data
+            })
+        }).catch(function(e){
+            if(e.response && e.response.status && e.response.status == 404){
+                _this.loadedSuccess = false;
+            }
+            console.log('TEST');
+        })
+    }
+
+    handleTabChange(e,data){
+        if(!this.state.staff || !this.state.staff.hasOwnProperty('staff_id')){
+            this.setState({
+                ...this.state,
+                salaries:[],
+                salariesLoaded:false,
+                expenses:[],
+                expensesLoaded:false
+            })
+
+            return;
+        }
+
+        //Salaries
+        if(data.activeIndex == 2){
+            if(this.state.salariesLoaded){
+                return;
+            }
+            if(!this.state.staff || !this.state.staff.hasOwnProperty('staff_id')){
+                this.setState({
+                    ...this.state,
+                    salaries:[]
+                })
+            }else{
+                axios.get(`${ROOT_URL}/api/hr/staff/` + this.state.staff.staff_id + `/salaries`,{
+                    headers: {
+                        authorization: localStorage.getItem('token')}
+                }).then((response) => {
+                    this.setState({
+                        ...this.state,
+                        salaries:response.data,
+                        salariesLoaded:true
+                    })
+                }).catch((error) => {
+                    console.log(error);
+                })
+            }
+
+        }else if(data.activeIndex == 4){
+            if(this.state.expensesLoaded){
+                return;
+            }
+
+            axios.get(`${ROOT_URL}/api/hr/staff/` + this.state.staff.staff_id + `/expenses`,{
+                headers: {
+                    authorization: localStorage.getItem('token')}
+            }).then((response) => {
+                this.setState({
+                    ...this.state,
+                    expenses:response.data,
+                    expensesLoaded:true
+                })
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
+        //console.log(e);
+        //console.log(data);
+    }
+
+    renderNotFound(){
+        return (
+            <h2>Сотрудник не найден</h2>
+        )
+    }
+
+    formatTimestamptToDate(v){
+        if(!v || v.length == 0){
+            return ''
+        }
+        return (
+            moment.utc(v).format("DD.MM.YYYY")
+        )
+    }
+
+    renderMainData(){
+        let stf = this.state.staff;
+        return (
+            <Table celled striped>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell>Фамилия</Table.Cell>
+                        <Table.Cell>{stf.lastname}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Имя</Table.Cell>
+                        <Table.Cell>{stf.firstname}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Отчество</Table.Cell>
+                        <Table.Cell>{stf.middlename}</Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row>
+                        <Table.Cell>ИИН</Table.Cell>
+                        <Table.Cell>{stf.iin}</Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row>
+                        <Table.Cell>Дата рождения</Table.Cell>
+                        <Table.Cell>{this.formatTimestamptToDate(stf.birthday)}</Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row>
+                        <Table.Cell>Пол</Table.Cell>
+                        <Table.Cell>{stf.gender == 'male'?'Мужской':'Женский'}</Table.Cell>
+                    </Table.Row>
+
+                </Table.Body>
+            </Table>
+        )
+    }
+
+    renderPassportData(){
+        let stf = this.state.staff;
+        return (
+            <Grid>
+                <Grid.Row columns={2}>
+                    <Grid.Column width={8}>
+
+                        <Grid celled>
+                            <Grid.Row>
+                                <Grid.Column width={5} textAlign={'right'}>Номер уд. личности</Grid.Column>
+                                <Grid.Column width={7}>{stf.passport_id}</Grid.Column>
+                            </Grid.Row>
+
+                            <Grid.Row>
+                                <Grid.Column width={5} textAlign={'right'}>Кем выдан</Grid.Column>
+                                <Grid.Column width={7}>{stf.passport_given_by}</Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column width={5} textAlign={'right'}>Дата выдачи</Grid.Column>
+                                <Grid.Column width={7}>{this.formatTimestamptToDate(stf.passport_given_date)}</Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column width={5} textAlign={'right'}>Срок действия уд.</Grid.Column>
+                                <Grid.Column width={7}>{this.formatTimestamptToDate(stf.passport_validity)}</Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Grid.Column>
+
+                    <Grid.Column width={8}>
+                        <Grid celled>
+                            <Grid.Row>
+                                <Grid.Column width={5} textAlign={'right'}>Номер паспорта</Grid.Column>
+                                <Grid.Column width={7}>{stf.passport_id}</Grid.Column>
+                            </Grid.Row>
+
+                            <Grid.Row>
+                                <Grid.Column width={5} textAlign={'right'}>Кем выдан (паспорт)</Grid.Column>
+                                <Grid.Column width={7}>{stf.passport_given_by}</Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column width={5} textAlign={'right'}>Дата выдачи (паспорт)</Grid.Column>
+                                <Grid.Column width={7}>{this.formatTimestamptToDate(stf.passport_given_date)}</Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column width={5} textAlign={'right'}>Срок действия (паспорт)</Grid.Column>
+                                <Grid.Column width={7}>{this.formatTimestamptToDate(stf.passport_validity)}</Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        )
+    }
+
+    renderContactData(){
+        return (
+            <h2>Contact</h2>
+        )
+    }
+
+    renderSalaryData(){
+        let tempContent = this.state.salaries.map(salary => {
+            return (
+                <Table.Row className={salary.prev == true ? 'error':(salary.next == true?'success':'')}>
+                    <Table.Cell>{salary.salaryId}</Table.Cell>
+                    <Table.Cell>{salary.statusName}</Table.Cell>
+                    <Table.Cell>{salary.companyName}</Table.Cell>
+                    <Table.Cell>{salary.branchName}</Table.Cell>
+                    <Table.Cell>{salary.departmentName}</Table.Cell>
+                    <Table.Cell>{salary.beginDate}</Table.Cell>
+                    <Table.Cell>{salary.endDate}</Table.Cell>
+                    <Table.Cell>{salary.positionName}</Table.Cell>
+                    <Table.Cell>{salary.amount}</Table.Cell>
+                    <Table.Cell>{salary.waers}</Table.Cell>
+                    <Table.Cell>{salary.note}</Table.Cell>
+                    <Table.Cell>{salary.payrollDate}</Table.Cell>
+                    <Table.Cell>
+
+                    </Table.Cell>
+                </Table.Row>
+            )
+        })
+        return (
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>SalaryId</Table.HeaderCell>
+                        <Table.HeaderCell>Статус</Table.HeaderCell>
+                        <Table.HeaderCell>Компания</Table.HeaderCell>
+                        <Table.HeaderCell>Филиал</Table.HeaderCell>
+                        <Table.HeaderCell>Департамент</Table.HeaderCell>
+                        <Table.HeaderCell>Дата начало</Table.HeaderCell>
+                        <Table.HeaderCell>Дата окончания</Table.HeaderCell>
+                        <Table.HeaderCell>Должность</Table.HeaderCell>
+                        <Table.HeaderCell>Оклад</Table.HeaderCell>
+                        <Table.HeaderCell>Валюта</Table.HeaderCell>
+                        <Table.HeaderCell>Примечание</Table.HeaderCell>
+                        <Table.HeaderCell>Дата выдачи</Table.HeaderCell>
+                        <Table.HeaderCell>Действия</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>{tempContent}</Table.Body>
+            </Table>
+        )
+    }
+
+    renderExpensesData(){
+        let tempContent = this.state.expenses.map(exp => {
+            return (
+                <Table.Row>
+                    <Table.Cell>{exp.id}</Table.Cell>
+                    <Table.Cell>{exp.typeName}</Table.Cell>
+                    <Table.Cell>{exp.amount}</Table.Cell>
+                    <Table.Cell>{exp.currency}</Table.Cell>
+                    <Table.Cell>{exp.description}</Table.Cell>
+                    <Table.Cell>{exp.expenseDate}</Table.Cell>
+                    <Table.Cell>
+
+                    </Table.Cell>
+                </Table.Row>
+            )
+        })
+        return (
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>ID</Table.HeaderCell>
+                        <Table.HeaderCell>Тип расхода</Table.HeaderCell>
+                        <Table.HeaderCell>Сумма</Table.HeaderCell>
+                        <Table.HeaderCell>Валюта</Table.HeaderCell>
+                        <Table.HeaderCell>Примечание</Table.HeaderCell>
+                        <Table.HeaderCell>Дата</Table.HeaderCell>
+                        <Table.HeaderCell>Действия</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>{tempContent}</Table.Body>
+            </Table>
+        )
+    }
+
+    renderProfile(){
+        const panes = [
+            {menuItem:'Основные данные',render:this.renderMainData},
+            {menuItem:'Паспортные данные',render:this.renderPassportData},
+            {menuItem:'Должности',render:this.renderSalaryData},
+            {menuItem:'Контакты',render:this.renderContactData},
+            {menuItem:'Расходы',render:this.renderExpensesData},
+            {menuItem:'Оф. данные',render:this.renderMainData},
+            {menuItem:'Файлы',render:this.renderMainData},
+            {menuItem:'Доп. данные',render:this.renderMainData},
+            {menuItem:'Баланс',render:this.renderMainData},
+            {menuItem:'Склад',render:this.renderMainData}
+        ]
+        return (
+            <Container fluid style={{ marginTop: '2em', marginBottom: '2em', paddingLeft: '2em', paddingRight: '2em'}}>
+                <Header as="h2" block>
+                    Карточка сотрудника / {this.state.staff.lastname} {this.state.staff.firstname}
+                </Header>
+                <Divider/>
+                <Tab panes={panes} menu={{ secondary: true, pointing: true }} onTabChange={this.handleTabChange} />
+            </Container>
+        )
+    }
+
+    render(){
+        if(this.loadedSuccess){
+            return this.renderProfile();
+        }
+        return this.renderNotFound()
+    }
+}
+
+export default ViewStaff;
