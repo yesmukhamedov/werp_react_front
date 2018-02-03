679f01f7a69a04ec3d6e207bbc67ef5ed3c36ef4
nurgissa <nurgisa@gmail.com>
2018-02-03 15:44:21 +0600

    Add highlighting of inactive and selected rows in the table

--- src/service/mainoperation/spList/components/spListTable.js
+++ src/service/mainoperation/spList/components/spListTable.js
@@ -6,6 +6,18 @@ import moment from 'moment'
 import {Checkbox, Icon} from 'semantic-ui-react'
 
 class SpListTable extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedIdx: undefined
+        }
+        this.handleAttributeChange = this.handleAttributeChange.bind(this)
+    }
+
+    handleAttributeChange(field, value) {
+        console.log("FIELD", field, "VALUE", value)
+    }
+
     render() {
         const columns = [
             {
@@ -58,7 +70,6 @@ class SpListTable extends Component {
                 Header: "Статус",
                 accessor: 'active',
                 Cell: props => {
-                        console.log(props, props); 
                         const {id, active} = props.original;
                         return <Checkbox 
                                     slider 
@@ -92,7 +103,33 @@ class SpListTable extends Component {
                     pageText="Страница"
                     ofText="из"
                     rowsText="записей"
-                    className="-striped -highlight" />);
+                    className="-highlight"
+                    getTrProps={(state, rowInfo , column) => {
+                        return {
+                            
+                            style: {
+                               background: (rowInfo == undefined ? '' : 
+                                (this.state.selectedIdx === rowInfo.index ? 'rgba(60, 143, 255, 0.5)' : 
+                                    (!rowInfo.original.active ? 'rgba(241, 241, 241, 0.8)' : "")))
+                            },
+                            onClick: (e, handleOriginal) => {
+                                console.log('A Td Element was clicked!')
+                                console.log('it produced this event:', e)
+                                console.log('It was in this column:', column)
+                                console.log('It was in this row:', rowInfo)
+                                
+                                let { index, original } = rowInfo
+                            
+
+                                // IMPORTANT! React-Table uses onClick internally to trigger
+                                // events like expanding SubComponents and pivots.
+                                // By default a custom 'onClick' handler will override this functionality.
+                                // If you want to fire the original onClick handler, call the
+                                // 'handleOriginal' function.
+                                this.setState({...this.state, selectedIdx: index})
+                            }
+                       }
+                    }} />);
     }
 }
 
