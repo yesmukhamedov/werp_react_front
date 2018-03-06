f0d6577bfcd7d2bac4061972c52b5129022103d2
nurgissa <nurgisa@gmail.com>
2018-03-06 15:17:31 +0600

    Add data to purchases panel from backend

--- src/testComponent/mainoperation/newIssue/components/PurchasesPanel/PurchasesPanelDisplay.jsx
+++ src/testComponent/mainoperation/newIssue/components/PurchasesPanel/PurchasesPanelDisplay.jsx
@@ -2,51 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Segment, Label, Form, Table, Header } from 'semantic-ui-react';
 
-const PurchasesPanelDisplay = props => (
-  <Segment raised>
-    <Label color="olive" ribbon>
-      Другие покупки
-    </Label>
-    <Grid divided="vertically">
-      <Grid.Row columns={2}>
-        <Grid.Column>
-          <Header as="h4">Продукты</Header>
-          <Table celled structured>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell># Договора</Table.HeaderCell>
-                <Table.HeaderCell>Продукт</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>1123123</Table.Cell>
-                <Table.Cell>Cebilon</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>2341234</Table.Cell>
-                <Table.Cell>Roboclean</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
-        </Grid.Column>
-        <Grid.Column>
-          <Form>
-            <Form.Field
-              label="Дата возврата"
-              control="input"
-              defaultValue={props.returnDate}
-              width="6"
-            />
-          </Form>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Segment>
-);
+const PurchasesPanelDisplay = (props) => {
+  const { otherPurchases, cancelledAt } = props;
+  return (
+    <Segment raised>
+      <Label color="olive" ribbon>
+        Другие покупки
+      </Label>
+      <Grid divided="vertically">
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <Header as="h4">Продукты</Header>
+            <Table celled structured>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell># Договора</Table.HeaderCell>
+                  <Table.HeaderCell>Продукт</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              {
+                otherPurchases &&
+                otherPurchases.map(item => (
+                  <Table.Body>
+                    <Table.Row key={item.contractNumber}>
+                      <Table.Cell>{item.contractNumber}</Table.Cell>
+                      <Table.Cell>{item.productName}</Table.Cell>
+                    </Table.Row>
+                  </Table.Body>
+                ))
+              }
+            </Table>
+          </Grid.Column>
+          <Grid.Column>
+            <Form>
+              <Form.Field
+                label="Дата возврата"
+                control="input"
+                value={cancelledAt}
+                width="6"
+              />
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  );
+};
 
 PurchasesPanelDisplay.propTypes = {
-  returnDate: PropTypes.string.isRequired,
+  otherPurchases: PropTypes.array,
+  cancelledAt: PropTypes.string.isRequired,
 };
 
 export default PurchasesPanelDisplay;
