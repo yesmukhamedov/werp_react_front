e80b53e3505e7e5b7c5c9d49dcc04f8497efde7a
numatay <nurgisa@gmail.com>
2018-12-12 11:06:44 +0600

    Add more columns to summare table

--- src/dit/tasks/dtskc/pages/DtskcSummaryDisplay.jsx
+++ src/dit/tasks/dtskc/pages/DtskcSummaryDisplay.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Table } from 'semantic-ui-react';
+import { constructFullName } from '../../../../utils/helpers';
 
 const DtskcSummary = (props) => {
   const { location = {} } = props;
@@ -21,24 +22,18 @@ const DtskcSummary = (props) => {
           <Table.Row>
             <Table.HeaderCell>Номер задачи</Table.HeaderCell>
             <Table.HeaderCell>Тема</Table.HeaderCell>
-            <Table.HeaderCell>Назначена</Table.HeaderCell>
+            <Table.HeaderCell>Исполнитель</Table.HeaderCell>
+            <Table.HeaderCell>Начальник исполнитея</Table.HeaderCell>
+            <Table.HeaderCell>Филиал</Table.HeaderCell>
+            <Table.HeaderCell>Отдел</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {state.createdTasks ? (
-            state.createdTasks.map(task => (
-              <Table.Row>
-                <Table.Cell>
-                  {/* <Link to={`/dit/task/dtskedit/${task.id}`}>{task.id}</Link> */}
-                  {task.id}
-                </Table.Cell>
-                <Table.Cell>{task.title}</Table.Cell>
-                <Table.Cell textAlign="right">{JSON.stringify(task)}</Table.Cell>
-              </Table.Row>
-            ))
+            state.createdTasks.map(task => renderTaskAssignee(task))
           ) : (
             <Table.Row>
-              <Table.Cell colSpan="3">
+              <Table.Cell colSpan="6" textAlign="center">
                 Нет зачач для отображения
               </Table.Cell>
             </Table.Row>
@@ -46,6 +41,29 @@ const DtskcSummary = (props) => {
         </Table.Body>
       </Table>
     </Container>
+  );
+};
+
+const renderTaskAssignee = (task) => {
+  const { recipient } = task;
+  const {
+    assignee,
+    assigneesManager,
+    branch,
+    department,
+  } = recipient;
+  return (
+    <Table.Row>
+      <Table.Cell>
+        {/* <Link to={`/dit/task/dtskedit/${task.id}`}>{task.id}</Link> */}
+        {task.id}
+      </Table.Cell>
+      <Table.Cell>{task.title}</Table.Cell>
+      <Table.Cell>{(assignee ? constructFullName(assignee) : <span>&mdash;</span>)}</Table.Cell>
+      <Table.Cell>{(assigneesManager ? assigneesManager.value : <span>&mdash;</span>)}</Table.Cell>
+      <Table.Cell>{(branch ? branch.value : <span>&mdash;</span>)}</Table.Cell>
+      <Table.Cell>{(department ? department.value : <span>&mdash;</span>)}</Table.Cell>
+    </Table.Row>
   );
 };
 export default DtskcSummary;
