65512f6ba27ac58087c212926422f419890b000b
numatay <nurgisa@gmail.com>
2018-12-12 19:28:18 +0600

    Add prettier to format codebase according to conventions Add husky and pretty-quick to autoformat staged code before commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-  extends: "airbnb",
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     localStorage: true,
-    document: true
+    document: true,
   },
-  rules:{
-    "linebreak-style": 0,
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], 
-    "react/require-default-props": [0],
-    "react/forbid-prop-types": [0],
-    "import/prefer-default-export": "off",
-  }
+  rules: {
+    'linebreak-style': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/require-default-props': [0],
+    'react/forbid-prop-types': [0],
+    'import/prefer-default-export': 'off',
+  },
 };
