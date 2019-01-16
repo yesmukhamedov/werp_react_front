75dcef61466d531638a7ba4ddc4cf1e2a646af67
numatay <nurgisa@gmail.com>
2019-01-16 17:15:43 +0600

    Fix task type code in DTSKC

--- src/dit/tasks/dtskc/actions/index.js
+++ src/dit/tasks/dtskc/actions/index.js
@@ -25,7 +25,7 @@ const createTaskUrl = `${ROOT_URL}/api/tasks`;
 
 export function fetchUsers(args) {
   const { branchId, bukrs } = args;
-  return (dispatch) => {
+  return dispatch => {
     axios
       .get(`${assigneesUrl}&branchId=${branchId}&bukrs=${bukrs}`, {
         headers: { authorization: localStorage.getItem('token') },
@@ -42,7 +42,7 @@ export function fetchUsers(args) {
           payload: { ...assigneesOpts },
         });
       })
-      .catch((error) => {
+      .catch(error => {
         if (error.response) {
           dispatch(notify('error', error.response.data.message, 'Ошибка'));
         }
@@ -51,7 +51,7 @@ export function fetchUsers(args) {
 }
 
 export function fetchReferences(lang) {
-  return (dispatch) => {
+  return dispatch => {
     axios
       .all([
         GET(departmentsUrl),
@@ -60,64 +60,75 @@ export function fetchReferences(lang) {
         GET(managersUrl),
         GET(groupsUrl),
       ])
-      .then(axios.spread((
-        { data: deptList },
-        { data: statusList },
-        { data: taskTypeList },
-        { data: managersList },
-        { data: groupList },
-      ) => {
-        const deptOpts = deptList.map(item => ({
-          key: item.dep_id,
-          value: {
-            depId: item.dep_id,
-            id: item.id,
-            ru: item.name_ru,
-            en: item.name_en,
-            tr: item.name_tr,
+      .then(
+        axios.spread(
+          (
+            { data: deptList },
+            { data: statusList },
+            { data: taskTypeList },
+            { data: managersList },
+            { data: groupList },
+          ) => {
+            const deptOpts = deptList.map(item => ({
+              key: item.dep_id,
+              value: {
+                depId: item.dep_id,
+                id: item.id,
+                ru: item.name_ru,
+                en: item.name_en,
+                tr: item.name_tr,
+              },
+              text:
+                lang === 'ru'
+                  ? item.name_ru
+                  : lang === 'en'
+                  ? item.name_en
+                  : item.name_tr,
+            }));
+
+            const statusOpts = statusList.map(item => ({
+              key: item.id,
+              value: item.id,
+              text: item[lang],
+            }));
+
+            const taskTypeOpts = taskTypeList.map(item => ({
+              key: item.code,
+              value: item.code,
+              text: lang === 'ru' ? item.ru : lang === 'en' ? item.en : item.tr,
+            }));
+
+            const managerOpts = managersList.map(
+              ({ id, department, user }) => ({
+                key: id,
+                departmentId: department.id,
+                value: user,
+                text: `${constructFullName(user)} - ${department[lang]}`,
+              }),
+            );
+
+            const groupOpts = groupList.map(({ groupId, groupName }) => ({
+              key: groupId,
+              value: groupId,
+              text: groupName,
+            }));
+
+            const directories = {
+              deptOptions: _.mapKeys(deptOpts, 'key'),
+              statusOptions: _.mapKeys(statusOpts, 'key'),
+              taskTypeOptions: _.mapKeys(taskTypeOpts, 'key'),
+              managerOptions: _.mapKeys(managerOpts, 'key'),
+              groupOptions: _.mapKeys(groupOpts, 'key'),
+            };
+
+            dispatch({
+              type: DTSKC_FETCH_REFERENCES,
+              payload: directories,
+            });
           },
-          text: lang === 'ru' ? item.name_ru : lang === 'en' ? item.name_en : item.name_tr,
-        }));
-
-        const statusOpts = statusList.map(item => ({
-          key: item.id,
-          value: item.id,
-          text: item[lang],
-        }));
-
-        const taskTypeOpts = taskTypeList.map(item => ({
-          key: item.departmentId,
-          value: item.code,
-          text: lang === 'ru' ? item.ru : lang === 'en' ? item.en : item.tr,
-        }));
-
-        const managerOpts = managersList.map(({ id, department, user }) => ({
-          key: id,
-          departmentId: department.id,
-          value: user,
-          text: `${constructFullName(user)} - ${department[lang]}`,
-        }));
-
-        const groupOpts = groupList.map(({ groupId, groupName }) => ({
-          key: groupId,
-          value: groupId,
-          text: groupName,
-        }));
-
-        const directories = {
-          deptOptions: _.mapKeys(deptOpts, 'key'),
-          statusOptions: _.mapKeys(statusOpts, 'key'),
-          taskTypeOptions: _.mapKeys(taskTypeOpts, 'key'),
-          managerOptions: _.mapKeys(managerOpts, 'key'),
-          groupOptions: _.mapKeys(groupOpts, 'key'),
-        };
-
-        dispatch({
-          type: DTSKC_FETCH_REFERENCES,
-          payload: directories,
-        });
-      }))
-      .catch((error) => {
+        ),
+      )
+      .catch(error => {
         if (error.response) {
           dispatch(notify('error', error.response.data.message, 'Ошибка'));
           console.log(error.response, 'Ошибка');
@@ -150,10 +161,12 @@ export function createTask(formValues, successCallback) {
       id: formValues.initiatorManager.id,
     },
     estimatedAt: moment.utc(formValues.estimatedAt, 'DD.MM.YYYY').format(),
-    attachment: (formValues.uploadList.length > 0 ?
-      {
-        attachmentJson: JSON.stringify(formValues.uploadList),
-      } : null),
+    attachment:
+      formValues.uploadList.length > 0
+        ? {
+            attachmentJson: JSON.stringify(formValues.uploadList),
+          }
+        : null,
   };
 
   console.log('ALL_RECIPIENTS', formValues.allRecipients);
@@ -163,13 +176,13 @@ export function createTask(formValues, successCallback) {
       authorization: localStorage.getItem('token'),
     },
   });
-  return (dispatch) => {
+  return dispatch => {
     request
       .then(({ data }) => {
         if (successCallback) successCallback(data);
         dispatch(notify('success', 'Задача успешно создана.', 'Успешно'));
       })
-      .catch((error) => {
+      .catch(error => {
         if (error.response) {
           dispatch(notify('error', error.response.data.message, 'Ошибка'));
         }
