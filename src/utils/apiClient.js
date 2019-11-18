bad73584e168085a1de409ec46c03c73440610fc
nurgali.yesmukhamdov <nurgali.yesmukhamdov@gmail.com>
2019-11-18 17:50:44 +0600

    LAST_QUERY_TIME added

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create();
 
 axiosInstance.interceptors.request.use(config => {
-  config.headers.Authorization = localStorage.getItem('token');
+  config.withCredentials = true;
+  let token = Cookies.get(
+    process.env.REACT_APP_LEGACY_COOKIE_PARAMS_JWT_TOKEN_NAME,
+  );
+  if (token) {
+    config.headers['authorization'] = token;
+  } else {
+    config.headers['authorization'] = '';
+  }
+
   config.headers['Content-Language'] = localStorage.getItem('language');
   return config;
 });
