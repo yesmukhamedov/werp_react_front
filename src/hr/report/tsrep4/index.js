8784ae50083a2724f907f664187da9681557bf10
Zhaksylyk <s.jako@mail.ru>
2019-11-28 10:55:10 +0600

    Added excel export tsrep

--- src/hr/report/tsrep4/index.js
+++ src/hr/report/tsrep4/index.js
@@ -94,13 +94,11 @@ function TSRep4(props) {
     excelHeaders.push(messages['brnch']);
     excelHeaders.push(messages['recommender']);
     excelHeaders.push(messages['recommenderPositionName']);
-    excelHeaders.push(messages['applicantName']);
-    excelHeaders.push(messages['applicantPositionName']);
-    excelHeaders.push(messages['applicantBeginDate']);
-    excelHeaders.push(messages['saleCount']);
-    excelHeaders.push(messages['recomenderBonus']);
+    excelHeaders.push(messages['countEmpAplTs']);
+    excelHeaders.push(messages['count_laidoff_employees']);
+    excelHeaders.push(messages['count_remaining_employees']);
     excelDownload(
-      '/api/hr/tsrep/monthly/excel',
+      'hr/tsrep/firremained/excel',
       'tsrep.xls',
       'outputTable',
       props.dynamicObject,
