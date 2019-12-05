c6715995771022711c0e52065a9f26fcab6249c2
Zhaksylyk <s.jako@mail.ru>
2019-12-05 10:27:55 +0600

    Install old version react-table 6.10.3

--- src/service/mainoperation/smsetct/index.js
+++ src/service/mainoperation/smsetct/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Segment } from 'semantic-ui-react';
 import 'react-table/react-table.css';
 
-const Smsetct = () = {
+function Smsetct() {
   return (
     <Segment>
       <h1>Welcome Smsetct</h1>
--- src/service/report/srlsm/index.js
+++ src/service/report/srlsm/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Segment } from 'semantic-ui-react';
 import 'react-table/react-table.css';
 
-const Srlsm = () => {
+function Srlsm() {
   return (
     <Segment>
       <h1>Welcome Srlsm</h1>
     </Segment>
   );
-};
+}
 
 export default Srlsm;
