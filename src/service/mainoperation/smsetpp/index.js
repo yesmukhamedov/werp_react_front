eb7a9fbe514678c3b6f7854aaff8f7941dee2ade
Sultan <sembeksultan23@gmail.com>
2019-12-06 15:45:25 +0600

    Sultan test commit and push

--- src/service/mainoperation/smsetpp/index.js
+++ src/service/mainoperation/smsetpp/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Dropdown } from 'semantic-ui-react';
 
 const Smsetpp = () => {
   return (
     <Segment>
-      <h1>Welcome Smsetpp</h1>
-      <h2>Sultan</h2>
+      <div className="settingPrice">
+        <div className="ui container">
+          <div className="flex-container">
+            <h1>Настройка цен и премии сервис услуг</h1>
+            <button className="ui blue button" id="addPrice">
+              <i aria-hidden="true" class="add square icon"></i>Добавить новую
+              цену
+            </button>
+          </div>
+          <Dropdown
+            selection
+            options={options}
+            placeholder="Компания"
+            onChange={handleClick}
+          />
+          <Dropdown
+            selection
+            options={options}
+            placeholder="Страна"
+            id="secondDropdown"
+          />
+          <button className="ui blue tiny button" id="addPrice2">
+            Поиск
+          </button>
+        </div>
+      </div>
     </Segment>
   );
 };
