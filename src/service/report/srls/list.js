29c1b74bb304e2b1d7cb7d9f66ce7bd9a0edaecd
Zhaksylyk <s.jako@mail.ru>
2019-12-19 12:58:32 +0600

    Изменение транзакции SRLS

---
+++ src/service/report/srls/list.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import matchSorter from 'match-sorter';
+import { Segment } from 'semantic-ui-react';
+
+export default function List(props) {
+  const { dynamicObject, messages } = props;
+
+  let columns = [
+    {
+      Header: messages['brnch'],
+      accessor: 'branchId',
+      filterMethod: (filter, rows) =>
+        matchSorter(rows, filter.value, { keys: ['branchId'] }),
+      filterAll: true,
+      width: 180,
+      maxWidth: 200,
+      minWidth: 100,
+    },
+    {
+      Header: 'Date',
+      accessor: 'dateOpen',
+      filterMethod: (filter, rows) =>
+        matchSorter(rows, filter.value, { keys: ['dateOpen'] }),
+      filterAll: true,
+      width: 300,
+      maxWidth: 300,
+      minWidth: 100,
+    },
+    {
+      Header: messages['recommenderPositionName'],
+      accessor: 'recommenderPositionName',
+      filterMethod: (filter, rows) =>
+        matchSorter(rows, filter.value, { keys: ['recommenderPositionName'] }),
+      filterAll: true,
+      width: 100,
+      maxWidth: 200,
+      minWidth: 100,
+    },
+    {
+      Header: messages['applicantName'],
+      accessor: 'applicantName',
+      filterMethod: (filter, rows) =>
+        matchSorter(rows, filter.value, { keys: ['applicantName'] }),
+      filterAll: true,
+      width: 300,
+      maxWidth: 300,
+      minWidth: 100,
+    },
+    {
+      Header: messages['applicantPositionName'],
+      accessor: 'applicantPositionName',
+      filterMethod: (filter, rows) =>
+        matchSorter(rows, filter.value, { keys: ['applicantPositionName'] }),
+      filterAll: true,
+      width: 150,
+      maxWidth: 200,
+      minWidth: 100,
+    },
+    {
+      Header: messages['applicantBeginDate'],
+      accessor: 'tsDate',
+      filterMethod: (filter, rows) =>
+        matchSorter(rows, filter.value, { keys: ['tsDate'] }),
+      filterAll: true,
+      width: 150,
+      maxWidth: 200,
+      minWidth: 100,
+    },
+    {
+      Header: messages['saleCount'],
+      accessor: 'saleCount',
+      filterMethod: (filter, rows) =>
+        matchSorter(rows, filter.value, { keys: ['saleCount'] }),
+      filterAll: true,
+      width: 150,
+      maxWidth: 200,
+      minWidth: 100,
+    },
+    {
+      Header: messages['recomenderBonus'],
+      accessor: 'recomenderBonus',
+      filterMethod: (filter, rows) =>
+        matchSorter(rows, filter.value, { keys: ['recomenderBonus'] }),
+      filterAll: true,
+      width: 150,
+      maxWidth: 200,
+      minWidth: 100,
+    },
+  ];
+
+  return (
+    <div>
+      (
+      <ReactTable
+        filterable
+        columns={columns}
+        data={dynamicObject}
+        resolveData={data => data.map(row => row)}
+        rowsText={messages['rowsText']}
+        pageText={messages['pageText']}
+        ofText={messages['ofText']}
+        previousText={messages['previousText']}
+        nextText={messages['nextText']}
+        noDataText={messages['loadingText']}
+      />
+      )
+    </div>
+  );
+}
