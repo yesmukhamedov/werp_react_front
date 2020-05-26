e1081868c18820284eb7c8ea8ac0dd6060a16bea
Zhaksylyk <s.jako@mail.ru>
2020-05-26 20:00:24 +0600

    Srls and srlsm

--- src/service/report/srlsm/srlsmAction.js
+++ src/service/report/srlsm/srlsmAction.js
@@ -3,6 +3,7 @@ import { handleError } from '../../../general/notification/notification_action';
 import { modifyLoader } from '../../../general/loader/loader_action';
 
 export const FETCH_SRLSM = 'FETCH_SRLSM';
+export const FETCH_SERVICE_TYPE_LIST = 'FETCH_SERVICE_TYPE_LIST';
 
 const errorTable = JSON.parse(localStorage.getItem('errorTableString'));
 const language = localStorage.getItem('language');
@@ -17,6 +18,25 @@ export const fetchSrlsm = param => {
         dispatch({
           type: FETCH_SRLSM,
           data,
+        });
+      })
+      .catch(error => {
+        dispatch(modifyLoader(false));
+        handleError(error, dispatch);
+      });
+  };
+};
+
+export const fetchServiceTypeList = param => {
+  return function(dispatch) {
+    dispatch(modifyLoader(true));
+    doGet(`smcs/getServiceTypeList`, param)
+      .then(({ data }) => {
+        //console.log(data, 'ACTION');
+        dispatch(modifyLoader(false));
+        dispatch({
+          type: FETCH_SERVICE_TYPE_LIST,
+          data: data,
         });
       })
       .catch(error => {
--- src/service/report/srlsm/srlsmReducer.js
+++ src/service/report/srlsm/srlsmReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_SRLSM } from './srlsmAction';
+import { FETCH_SRLSM, FETCH_SERVICE_TYPE_LIST } from './srlsmAction';
 
 const INITIAL_STATE = {
   dynamicObject: [],
@@ -7,9 +7,17 @@ const INITIAL_STATE = {
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case FETCH_SRLSM:
+      console.log('REDUCE SRLSM', action.data.data);
       return {
         ...state,
-        srlsmList: [...action.data.data.data],
+        srlsmListData: [...action.data.data.listData.data],
+        srlsmListSum: { ...action.data.data.listSum },
+        srlsmTotalPages: action.data.data.listData.totalPages,
+      };
+    case FETCH_SERVICE_TYPE_LIST:
+      return {
+        ...state,
+        serviceTypeList: [...action.data.data],
       };
 
     default:
