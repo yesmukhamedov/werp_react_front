5f5d7c117450cada60c864c8a4e07c18d0817dba
nurgali.yesmukhamdov <nurgali.yesmukhamdov@gmail.com>
2020-09-29 10:59:32 +0600

    changed params attribute in requests

--- src/hr/mainoperation/document/components/HrDocCreatePage.js
+++ src/hr/mainoperation/document/components/HrDocCreatePage.js
@@ -277,6 +277,8 @@ class HrDocCreatePage extends Component {
   };
 
   handleItemChange = (index, fieldName, fieldValue) => {
+    console.log(fieldName, fieldValue);
+
     let doc = Object.assign({}, this.state.localDocument);
     let items = doc.items || [];
     if (!items[index]) {
