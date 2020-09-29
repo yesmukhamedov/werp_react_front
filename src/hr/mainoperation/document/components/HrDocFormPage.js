d0ffc809a6fd66dc3651deb16ed77f9deafe91e5
nurgali.yesmukhamdov <nurgali.yesmukhamdov@gmail.com>
2020-09-29 11:44:12 +0600

    changed params attribute in requests

--- src/hr/mainoperation/document/components/HrDocFormPage.js
+++ src/hr/mainoperation/document/components/HrDocFormPage.js
@@ -321,7 +321,7 @@ class HrDocFormPage extends Component {
     console.log(fieldName, fieldValue);
     //console.log(fieldName, fieldValue.getTime());
     console.log(fieldName, fieldValue.toDate());
-    console.log(fieldName, fieldValue.toDate().getMilliseconds());
+    console.log(fieldName, fieldValue.toDate().valueOf());
 
     if (fieldName === 'beginDate' || fieldName === 'endDate') {
       if (fieldValue) {
