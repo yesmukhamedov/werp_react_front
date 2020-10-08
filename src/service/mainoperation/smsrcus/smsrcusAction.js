b24d085a4a84d8e10c52389b7e9982eac978ac38
Zhaksylyk <s.jako@mail.ru>
2020-10-08 13:23:33 +0600

    Fixed bugs SMSRCUS

--- src/service/mainoperation/smsrcus/smsrcusAction.js
+++ src/service/mainoperation/smsrcus/smsrcusAction.js
@@ -7,12 +7,13 @@ import { modifyLoader } from '../../../general/loader/loader_action';
 
 //План по замене картриджей
 export const FETCH_SMSRCUS_LIST = 'FETCH_SMSRCUS_LIST';
+export const FETCH_SMSRCUS_BLACK_LIST = 'FETCH_SMSRCUS_BLACK_LIST';
 export const CLEAR_SMSRCUS_LIST = 'CLEAR_SMSRCUS_LIST';
+export const CLEAR_SMSRCUS_BLACK_LIST = 'CLEAR_SMSRCUS_BLACK_LIST';
 
 // const errorTable = JSON.parse(localStorage.getItem('errorTableString'));
 // const language = localStorage.getItem('language');
 
-//План по замене картриджей
 export const fetchSmsrcusList = (param, setFunc) => {
   return function(dispatch) {
     dispatch(modifyLoader(true));
@@ -31,11 +32,36 @@ export const fetchSmsrcusList = (param, setFunc) => {
       });
   };
 };
+export const fetchSmsrcusBlackList = (param, setFunc) => {
+  return function(dispatch) {
+    dispatch(modifyLoader(true));
+    doGet(`smsrcus/listCRMBlackClients`, param)
+      .then(({ data }) => {
+        setFunc();
+        dispatch(modifyLoader(false));
+        dispatch({
+          type: FETCH_SMSRCUS_BLACK_LIST,
+          payload: data,
+        });
+      })
+      .catch(error => {
+        dispatch(modifyLoader(false));
+        handleError(error, dispatch);
+      });
+  };
+};
 
 export function clearSmsrcusList() {
   return function(dispatch) {
     dispatch({
       type: CLEAR_SMSRCUS_LIST,
+    });
+  };
+}
+export function clearSmsrcusBlackList() {
+  return function(dispatch) {
+    dispatch({
+      type: CLEAR_SMSRCUS_BLACK_LIST,
     });
   };
 }
--- src/service/mainoperation/smsrcus/smsrcusReducer.js
+++ src/service/mainoperation/smsrcus/smsrcusReducer.js
@@ -1,4 +1,9 @@
-import { FETCH_SMSRCUS_LIST, CLEAR_SMSRCUS_LIST } from './smsrcusAction';
+import {
+  FETCH_SMSRCUS_LIST,
+  CLEAR_SMSRCUS_LIST,
+  CLEAR_SMSRCUS_BLACK_LIST,
+  FETCH_SMSRCUS_BLACK_LIST,
+} from './smsrcusAction';
 
 const INITIAL_STATE = {
   dynamicObject: [],
@@ -13,10 +18,20 @@ export default function(state = INITIAL_STATE, action) {
         ...state,
         smsrcusData: { ...action.payload.data },
       };
+    case FETCH_SMSRCUS_BLACK_LIST:
+      return {
+        ...state,
+        smsrcusBlackListData: { ...action.payload.data },
+      };
     case CLEAR_SMSRCUS_LIST:
       return {
         ...state,
         smsrcusData: {},
+      };
+    case CLEAR_SMSRCUS_BLACK_LIST:
+      return {
+        ...state,
+        smsrcusBlackListData: {},
       };
 
     default:
