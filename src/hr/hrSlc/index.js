3b13a2f55ba8b734480b9244fde8f5cac6a3d5a4
Zhaksylyk <s.jako@mail.ru>
2020-10-26 16:03:18 +0600

    Fixed bugs HRSLC

--- src/hr/hrSlc/index.js
+++ src/hr/hrSlc/index.js
@@ -14,56 +14,20 @@ import {
   Ref,
   Image,
   Checkbox,
+  Container,
 } from 'semantic-ui-react';
-import { YMaps, Map } from 'react-yandex-maps';
 
 const HrSlc = props => {
   const [visible, setVisible] = React.useState(false);
+  const { ymaps } = window;
+  console.log('ymaps', ymaps);
+  const [mapLoading, setMapLoading] = useState(false);
 
   return (
-    <Grid columns={1} style={{ height: '100%' }}>
-      <Grid.Column style={{ height: '100%' }}>
-        <Sidebar.Pushable as={Segment.Group} raised>
-          <Sidebar
-            as={Menu}
-            animation="overlay"
-            icon="labeled"
-            inverted
-            onHide={() => setVisible(false)}
-            vertical
-            // target={segmentRef}
-            visible={visible}
-            width="thin"
-          >
-            <Menu.Item as="a">Home</Menu.Item>
-            <Menu.Item as="a">Games</Menu.Item>
-            <Menu.Item as="a">Channels</Menu.Item>
-          </Sidebar>
-
-          <Segment>
-            <Header as="h3">
-              Application Content
-              <Checkbox
-                checked={visible}
-                label={{ children: <code>visible</code> }}
-                onChange={(e, data) => setVisible(data.checked)}
-              />
-            </Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-            <Header as="h3">Application Content</Header>
-          </Segment>
-        </Sidebar.Pushable>
-      </Grid.Column>
-    </Grid>
+    <Container>
+      <Segment>YANDEX MAPS</Segment>
+      <Segment>Map</Segment>
+    </Container>
   );
 };
 
