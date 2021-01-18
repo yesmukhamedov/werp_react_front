816a530d077f40d27257bece2cf684403a243cbf
azokarta <azokarta1988@gmail.com>
2021-01-18 16:47:51 +0600

    adding year options

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -47,6 +47,31 @@ export const YEAR_OPTIONS = [
     text: 2020,
     value: 2020,
   },
+  {
+    key: 2021,
+    text: 2021,
+    value: 2021,
+  },
+  {
+    key: 2022,
+    text: 2022,
+    value: 2022,
+  },
+  {
+    key: 2023,
+    text: 2023,
+    value: 2023,
+  },
+  {
+    key: 2024,
+    text: 2024,
+    value: 2024,
+  },
+  {
+    key: 2025,
+    text: 2025,
+    value: 2025,
+  },
 ];
 
 export const MONTH_OPTIONS = [
